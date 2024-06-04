64217d94050fa28ee786aafe61dc7ad25dbf02b9
comar1 <officialfranciscomar@gmail.com>
2024-06-04 10:33:07 +0800

    first

---
+++ pages/index.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="container">
+    <h1>Hello, Nuxt with SASS!</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HomePage',
+};
+</script>
+
+<style lang="scss" scoped>
+@import '~/assets/styles.scss';
+
+.container {
+  text-align: center;
+  padding: 20px;
+}
+</style>
